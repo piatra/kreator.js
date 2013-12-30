5f921e2b6d1816d1f9a450d683bee830eecb7673
Andrei Oprea <andrei.br92@gmail.com>
2013-12-30 21:44:52 +0200

    Fixed deprecated jade issue

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,14 @@
+var query = {};
+location.search.replace( /[A-Z0-9]+?=(\w*)/gi, function(a) {
+query[ a.split( '=' ).shift() ] = a.split( '=' ).pop();
+} );
+
+Reveal.initialize({
+	progress: true
+});
+
+google.load("webfont", "1");
+
 require.config({
 	paths: { 
 		jquery: './jquery-1.9.1.min'
